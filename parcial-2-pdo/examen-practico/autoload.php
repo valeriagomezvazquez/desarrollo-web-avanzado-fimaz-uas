6f3584c29ba528dc30e974ce00631003033f1811
ValeriaG <valee.gomezvazquez@gmail.com>
2026-04-27 07:01:11 -0700

    archivo index y autoload

---
+++ parcial-2-pdo/examen-practico/autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+spl_autoload_register(function ($class) {
+    $baseDir = __DIR__ . '/';
+
+    $class = str_replace('\\', '/', $class);
+    $file = $baseDir . $class . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+?>
